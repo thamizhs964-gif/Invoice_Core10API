ef2ee24e3261ea0cbfeb1090c127f4af3ee3011e
thamizhs964-gif <thamizhs964@gmail.com>
2026-08-09 19:00:09 +0530

    Removed Cached Files In Vendor

--- Invoice_API/obj/Debug/net10.0/Invoice_API.AssemblyInfo.cs
+++ Invoice_API/obj/Debug/net10.0/Invoice_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Invoice_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c91baea608f457d23ffd5ce9c8a8006460f59d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916b3f7a317ec11b022c1c2a21735dc26d55215d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Invoice_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Invoice_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
