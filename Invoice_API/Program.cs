d4ea5d4b315745cc6d85649cfd2d5a6646e14041
thamizhs964-gif <thamizhs964@gmail.com>
2026-08-09 12:34:17 +0530

    updated user branch

--- Invoice_API/Program.cs
+++ Invoice_API/Program.cs
@@ -14,9 +14,6 @@ using Invoice_API.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<ICategoryRepository, CategoryRepositories>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddAutoMapper(cfg => cfg.AddMaps(typeof(CategoryProfile).Assembly));
 
 builder.Services.AddScoped<IUsersRepository, UsersRepositories>();
 builder.Services.AddScoped<IUsersService, UsersService>();
